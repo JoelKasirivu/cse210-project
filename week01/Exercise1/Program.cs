b67ca1250d6828d0db4ce3b6f425c7a528c41e43
joelkasirivu <kasirivujoel256@gmail.com>
2025-05-09 11:32:04 +0300

    Asked for first and last name and display it int hthe console.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // This program asks the user for their first and last name and then prints it in a specific format.
+        // The format is: "Your name is: <last name>, <last name> <first name>"
+        Console.Write("What is your first name: ");
+        string first = Console.ReadLine();
+        
+
+        Console.Write("What is your last name: ");
+        string last = Console.ReadLine();
+        
+
+        Console.WriteLine($"Your name is:{last}, {first} {last}");
+
+
+
+
     }
 }
